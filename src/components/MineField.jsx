10cf0aa484a6a4e9d4bcbf64149b69802fb9d7f0
ruib07 <ruibarreto07@outlook.com>
2023-07-17 22:35:37 +0100

    Mines Game App

---
+++ src/components/MineField.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import Field from './Field';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#EEE',
+  },
+});
+
+export default props => {
+  const rows = props.board.map((row, r) => {
+    const columns = row.map((field, c) => {
+      return (
+        <Field
+          {...field}
+          key={c}
+          onOpen={() => props.onOpenField(r, c)}
+          onSelect={e => props.onSelectField(r, c)}
+        />
+      );
+    });
+    return (
+      <View key={r} style={{flexDirection: 'row'}}>
+        {columns}
+      </View>
+    );
+  });
+  return <View style={styles.container}>{rows}</View>;
+};
